d6e0a4946a83146e8dfa55e3b405d160bf97ed97
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2024-12-01 02:08:41 +0500

    Added stuff

---
+++ Graph-Algorithms/Graph.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Graph{
+    private int vertices;
+    private ArrayList<ArrayList<Integer>> adj;
+
+    public Graph(int vertices){
+        this.vertices = vertices;
+        adj = new ArrayList<>(vertices);
+
+        for(int i = 0; i < vertices; i++){
+            adj.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int u, int v, int w){
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
+
+    public int PrimsMST(){
+        int cost = 0;
+        boolean visited = new Set<Integer>(vertices);
+        
+    }
+
+}
